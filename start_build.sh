7e68691eb10d46f5cee16b30a0dc386c478e26a1
Redspitee <2152765100@qq.com>
2019-07-04 20:13:26 +0800

    Update start_build.sh

--- start_build.sh
+++ start_build.sh
@@ -2,6 +2,6 @@ yarn build
 git config --global user.name "redspitee"
 git config --global user.email "718352984@qq.com"
 git add .
-git commit -m "form start_build"
+git commit -m "from start_build"
 git pull origin master
 git push origin master
