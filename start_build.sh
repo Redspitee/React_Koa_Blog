b484d2046070491d6345c6b6e9d08f76fa3a5f8e
redspitee <718352984@qq.com>
2019-07-05 11:14:18 +0800

    0705 - update start_build.sh

--- start_build.sh
+++ start_build.sh
@@ -3,8 +3,8 @@ git add .
 git commit -m "0705 - update start_build.sh"
 git pull origin master
 git push origin master
-rm -rf server_blog/*
-cp -ri server/* server_blog
-cd server_blog
+rm -rf ../server_blog/*
+cp -ri server/* ../server_blog
+cd ../server_blog
 git pull origin pc
 git push origin pc
