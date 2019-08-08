cbd664fcd77c0e7a121c7342f8b1362666b15328
redspitee <718352984@qq.com>
2019-08-08 11:09:22 +0800

    0808

--- config/paths.js
+++ config/paths.js
@@ -69,7 +69,7 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('./'),
-  appBuild: resolveApp('server/www'),
+  appBuild: resolveApp('build/www'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
